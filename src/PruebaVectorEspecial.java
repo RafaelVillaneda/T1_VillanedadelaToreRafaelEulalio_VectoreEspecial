3027f2328dcfab6e6b8511eb164f41c8ed52624f
Rafael Eulalio Villaneda de la torre <Rafael.rafaeua2401@outlook.com>
2020-09-16 13:16:18 -0500

    Se modifico el proyecto agregando el punto 9 y terminandolo

--- src/PruebaVectorEspecial.java
+++ src/PruebaVectorEspecial.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 5)Obtener catidad de elementos..Completo
 6)Mostrar todos los elemntos....Completo
 7)Mostrar el primer elemnto.....Completo
-8)Mostrar el ultimo elemento
-9)Aumentar el tamaño del arreglo
+8)Mostrar el ultimo elemento....Completo
+9)Aumentar el tamaño del arreglo ..Completo
 10)Disminuir el tamaño del arreglo
 11)Insertar elemto en pocicion especifica
 12)eleminar el elemrnto de pocicion especifica
@@ -19,12 +19,24 @@ import java.util.Scanner;
 class VectorEspecial{
 	Scanner entrada=new Scanner(System.in);
 		//declaracion
-	int edades[];
+	private int edades[];
 	
 	public VectorEspecial(int tamaño) {
 		//	Creacion
 		this.edades=new int[tamaño];
 	}
+	
+	
+	public int[] getEdades() {
+		return edades;
+	}
+
+
+	public void setEdades(int[] edades) {
+		this.edades = edades;
+	}
+
+
 	public void llenarVector() {
 		for (int i = 0; i < edades.length; i++) {
 			System.out.println("Ingresa edad: "+(i+1)+" :");
@@ -50,12 +62,25 @@ class VectorEspecial{
 	public void mostrarUltimoElemento() {
 		System.out.println("El ultimo elemento es: "+edades[edades.length-1]);
 	}
-	
+	public void aumentarTamañoArreglo(int tamañoNuevo) {
+		boolean bandera=false;
+		do {
+		if(tamañoNuevo>edades.length) {
+			bandera=true;
+			edades=new int [tamañoNuevo];
+		}else {
+			System.out.println("Error el dato que ingresaste no es mayor el numero debe de ser mayor a: "+edades.length);
+			System.out.println("Ingresa el tamaño que deseas para aumentar el tamaño");
+			tamañoNuevo=entrada.nextInt();
+		}
+		}while(bandera==false);
+		
+	}
 }
 public class PruebaVectorEspecial {
 
 	public static void main(String[] args) {
-		
+		Scanner entrada=new Scanner(System.in);
 		
 		System.out.println("-------------1---------------");
 		VectorEspecial ve3=new VectorEspecial(3);
@@ -75,6 +100,12 @@ public class PruebaVectorEspecial {
 		ve3.mostrarPrimerElemento();
 		System.out.println("----------------8----------");
 		ve3.mostrarUltimoElemento();
+		System.out.println("----------------9----------");
+		System.out.println("Ingresa el tamaño que deseas para aumentar el tamaño");
+		int tamaño=entrada.nextInt();
+		ve3.aumentarTamañoArreglo(tamaño);
+		System.out.println("El tmaño a sido cambiado correctamente a: "+ve3.getEdades().length);
+		
 	}
 
 }
